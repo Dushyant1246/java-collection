4acf8f4b5184c0f71831a866f39a2e1615167eb7
Dushyant1246 <dushyant.dhmn808@gmail.com>
2025-03-28 15:42:26 +0530

    [Dushyant] ADD: Reverse a Queue

---
+++ src/main/java/com/bridgelabz/queueInterface/ReverseQueue.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.queueInterface;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class ReverseQueue {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+
+        System.out.println("Original Queue: " + queue);
+        reverseQueue(queue);
+        System.out.println("Reversed Queue: " + queue);
+    }
+
+    static void reverseQueue(Queue<Integer> queue) {
+        Stack<Integer> stack = new Stack<>();
+        while (!queue.isEmpty()) {
+            stack.push(queue.remove());
+        }
+        while (!stack.isEmpty()) {
+            queue.add(stack.pop());
+        }
+    }
+}
+/*
+Original Queue: [10, 20, 30]
+Reversed Queue: [30, 20, 10]
+ */
